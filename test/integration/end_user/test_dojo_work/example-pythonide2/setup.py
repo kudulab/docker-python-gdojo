bd6cab8acb23491f39aa4bf353e90a07332d91a5
Ewa Czechowska <ewa@ai-traders.com>
2019-06-26 23:45:44 +0200

    remove unused test python package and requirements files

--- test/integration/end_user/test_dojo_work/example-pythonide2/setup.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-from setuptools import setup, Command
-
-setup(name='example-pythonide2',
-      description='A insignificant project for the sake of tests',
-      version='1.22.5',
-      py_modules=['example']
-      )
